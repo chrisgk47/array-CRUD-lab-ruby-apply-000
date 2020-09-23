2b5da89c3b1f02c60cbafaa0907842754305b879
Christopher Kim <chris.kim1990@gmail.com>
2020-09-23 15:56:19 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -6,6 +6,6 @@ def create_an_array
   ["Matthew", "Mark", "Luke", 'John']
 end
 
-def add_element_to_end_of_array(array)
-  array = ["Matthew"] << "Mark"
+def add_element_to_end_of_array["Matthew"]
+  << "Mark"
 end
